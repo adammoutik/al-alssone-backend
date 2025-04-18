d3ac7d038364b113a994cb755a3e413d4744f5de
adammoutik <adamadamgamer@gmail.com>
2025-04-18 22:36:25 +0200

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+  .setTitle('School Payments API')
+  .setDescription('Management des paiements scolaires')
+  .addBearerAuth()
+  .build();
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`API documentation is available at: ${await app.getUrl()}/api`);
 }
 bootstrap();
