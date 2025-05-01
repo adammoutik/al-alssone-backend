010f175286e89a8ff4394af0c11870ef032fd2f8
adammoutik <adamadamgamer@gmail.com>
2025-05-02 01:53:15 +0200

    fixed errors

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,11 +3,15 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FeeSchema } from 'src/fees/entities/fee.entity';
+import { PaymentSchema } from './entities/payment.entity';
+import { StudentSchema } from 'src/students/entities/student.entity';
+import { FamilySchema } from 'src/families/entities/family.entity';
 
 @Module({
   imports: [MongooseModule.forFeature([
-      { name: 'Fee', schema: FeeSchema }])],
+      { name: 'Fee', schema: FeeSchema },{name:'Payment',schema:PaymentSchema},{name:'Student',schema:StudentSchema},{name:'Family',schema:FamilySchema}])],
   controllers: [PaymentsController],
   providers: [PaymentsService],
+  exports: [PaymentsService],
 })
 export class PaymentsModule {}
